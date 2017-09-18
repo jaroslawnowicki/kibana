5bffd30a488d70055dcdd8e1c4ff6034517d53ac
jaroslaw nowicki <jaroslaw.nowicki83@gmail.com>
2017-09-18 20:27:22 +0200

    init

---
+++ src/main/kotlin/it/jaroslaw/nowicki/KibanaApplication.kt
@@ -0,0 +1,11 @@
+package it.jaroslaw.nowicki
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KibanaApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KibanaApplication::class.java, *args)
+}
